0515d2b4a18d18f775591f1f683a9bbd7eb03217
741zxc606 <741zxc606@sina.com>
2016-11-10 00:36:23 +0800

    add HelloObjective-C

---
+++ HelloObjective-C/Hello Objective-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello Objective-C
+//
+//  Created by tesla422 on 16/11/9.
+//  Copyright © 2016年 tesla422. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, Objective-C!");
+    }
+    return 0;
+}
